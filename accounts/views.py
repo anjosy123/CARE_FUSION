12f30f5b007b82355c8c973dbaf2564e256b52c6
anjosy123 <anjosy1996@gmail.com>
2024-08-14 21:28:26 +0530

    second commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegisterForm
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
 def home(request):
     return render(request, 'index.html')
 def register(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+        else:
+            form = UserRegisterForm()
+        return render(request, 'register.html',{'form':form})
+    
