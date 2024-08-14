3d0778a6936289cab79233a54bdf6133b1de44c0
anjosy123 <anjosy1996@gmail.com>
2024-08-14 21:32:49 +0530

    second commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
-from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
-
-class CustomLoginView(LoginView):
-    template_name = 'login.html'
-    
-class CustomLogoutView(LogoutView):
-    template_name = 'logout.html'
 def home(request):
     return render(request, 'index.html')
 def register(request):
@@ -17,10 +10,3 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('login')
-        else:
-            form = UserRegisterForm()
-        return render(request, 'register.html',{'form':form})
-    
--- care_fusion/urls.py
+++ care_fusion/urls.py
@@ -17,13 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from accounts import views
-from accounts.views import register, CustomLoginView, CustomLogoutView
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('register/', register, name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
