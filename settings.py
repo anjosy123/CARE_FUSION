ccc7bb7679fb013daa5729b3149a554815ab2b28
Anjosy AJ <anjosy1996@gmail.com>
2025-03-04 21:16:46 +0530

    Solving Hosting issue...

--- settings.py
+++
@@ -1,8 +0,0 @@
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        ...
-    },
-] 
